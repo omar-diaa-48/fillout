5a95a5f7b67db375e00d16efecfc5cfcb48b65bb
omar <o.wakeel@intouch.com>
2024-03-02 14:04:00 +0200

    feat: extract logic to be in utilities file

--- src/utilities/index.ts
+++ src/utilities/index.ts
@@ -1,1 +1,104 @@
+import dayjs from "dayjs";
+import { FillOutFormSubmissionResponse, ResponseFiltersType } from "../interfaces";
+
 export const getFillOutUrl = (): string => `${process.env.FILL_OUT_API_BASE_URL}/${process.env.FILL_OUT_API_VERSION}`
+
+export const getParamsFromRequest = (query: Record<string, any>): [number, number, Record<string, any>] => {
+    const limit: number = query.limit ? !isNaN(parseInt(query.limit + '')) ? parseInt(query.limit + '') : 150 : 150;
+    const offset: number = query.offset ? !isNaN(parseInt(query.offset + '')) ? parseInt(query.offset + '') : 0 : 0;
+
+    const status = query.status;
+    const sort = query.sort;
+    const afterDate = query.afterDate;
+    const beforeDate = query.beforeDate;
+
+    const params: Record<string, any> = {
+        limit,
+        offset
+    }
+
+    if (afterDate) {
+        params.afterDate = afterDate
+    }
+
+    if (beforeDate) {
+        params.beforeDate = beforeDate
+    }
+
+    if (status) {
+        params.status = status
+    }
+
+    if (sort) {
+        params.sort = sort
+    }
+
+    return [limit, offset, params];
+}
+
+export const getFilteredResponses = (responses: FillOutFormSubmissionResponse[], filters: ResponseFiltersType): FillOutFormSubmissionResponse[] => {
+    const filteredResponses: FillOutFormSubmissionResponse[] = responses.filter((submission) => {
+        // Iterate over filters
+        return filters.every((filter) => {
+            // Check if the question with matching id exists in the response
+            const question = submission.questions.find((q) => q.id === filter.id);
+
+            if (question) {
+                // Apply condition check based on the filter, avoid strict data type validation in comparison
+                const valueType = !isNaN(parseFloat(question.value)) ? "number" : dayjs(question.value).isValid() ? "date" : "string"
+
+                switch (filter.condition) {
+
+                    case "equals":
+                        if (valueType === "date") {
+                            return dayjs(question.value).isSame(filter.value)
+                        }
+
+                        return question.value == filter.value;
+
+
+                    case "does_not_equal":
+                        if (valueType === "date") {
+                            return !dayjs(question.value).isSame(filter.value)
+                        }
+
+                        return question.value != filter.value;
+
+
+                    case "greater_than":
+                        switch (valueType) {
+                            case "date":
+                                return dayjs(question.value).isAfter(filter.value)
+                            case "number":
+                                return question.value > filter.value
+                            case "string":
+                            default:
+                                return false
+                        }
+
+
+                    case "less_than":
+                        switch (valueType) {
+                            case "date":
+                                return dayjs(question.value).isBefore(filter.value)
+                            case "number":
+                                return question.value < filter.value
+                            case "string":
+                            default:
+                                return false
+                        }
+
+
+                    default:
+                        return false;
+
+                }
+            }
+
+            // If question with filter id not found, return false
+            return false;
+        });
+    });
+
+    return filteredResponses;
+}
